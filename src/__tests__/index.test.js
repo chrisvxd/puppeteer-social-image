8bad772b36d0d7d23172e4ae8338588a7dae7a48
Chris Villa <chrisvilla@me.com>
2019-05-16 10:54:09 +0300

    feat: add support for custom templates

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -12,6 +12,79 @@ describe("puppeteer-social-image", () => {
         size: "facebook" // Defaults to twitter, as smallest
       });
 
+      // TODO sort this
+      expect(true);
+    });
+
+    it("must generate an image with a custom template", async () => {
+      const body = `<div class="Main">Hello, {{name}}!</div>`;
+
+      const styles = `
+        .Main {
+          align-items: center;
+          background: rebeccapurple;
+          display: flex;
+          justify-content: center;
+          font-family: "Avenir Next", "Lato", "Helvetica Neue", sans-serif;
+          font-size: 128px;
+          font-weight: 700;
+          padding: 32px;
+          text-align: center;
+          width: 100%;
+          height: 100%;
+        }
+      `;
+
+      await getImage({
+        templateBody: body,
+        templateStyles: styles,
+        templateParams: {
+          name: "Tony"
+        },
+        output: "image.png" // Optional, if supplied write to path
+      });
+
+      expect(true);
+    });
+
+    it("must generate an image with a custom template when providing multiple custom templates", async () => {
+      const body = `<div class="Main">Hello, {{name}}!</div>`;
+
+      const styles = `
+        .Main {
+          align-items: center;
+          background: rebeccapurple;
+          color: white;
+          display: flex;
+          justify-content: center;
+          font-family: "Avenir Next", "Lato", "Helvetica Neue", sans-serif;
+          font-size: 128px;
+          font-weight: 700;
+          padding: 32px;
+          text-align: center;
+          width: 100%;
+          height: 100%;
+        }
+      `;
+
+      await getImage({
+        customTemplates: {
+          foo: {
+            body,
+            styles
+          },
+          bar: {
+            body,
+            styles
+          }
+        },
+        template: "bar",
+        templateParams: {
+          name: "Tony"
+        },
+        output: "image.png" // Optional, if supplied write to path
+      });
+
       expect(true);
     });
   });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "@babel/polyfill";
 import path from "path";
 import puppeteer from "puppeteer";
-import templates from "./templates";
+import templates, { baseTemplate } from "./templates";
 import tempy from "tempy";
+import { compile } from "handlebars";
 
 const sizeMap = {
   facebook: { width: 1200, height: 630 },
@@ -24,12 +25,16 @@ export const getImage = async ({
   output,
   size = "twitter",
   template = "basic",
-  templateParams
+  templateParams = {},
+  templateBody,
+  templateStyles = "",
+  customTemplates = {}
 }) => {
   // Resolve preferences
   const _size = sizeMap[size];
   const { width, height } = _size;
-  const buildTemplate = templates[template];
+  const customTemplate = customTemplates[template];
+  const buildTemplate = template && templates[template];
   const ext = path
     .extname(output)
     .slice(1)
@@ -44,8 +49,24 @@ export const getImage = async ({
     height
   });
 
-  // Build template and set to puppeteer
-  const html = buildTemplate({ templateParams, size: _size });
+  // Using template builders instead of handlebars templates allows
+  // us to hide size, body and styles from the user template
+
+  const usingCustomTemplate =
+    typeof customTemplate !== "undefined" ||
+    typeof templateBody !== "undefined";
+  const customBody = (customTemplate && customTemplate.body) || templateBody;
+  const customStyles =
+    (customTemplate && customTemplate.styles) || templateStyles;
+
+  const html = usingCustomTemplate
+    ? baseTemplate({
+        body: compile(customBody)(templateParams),
+        height: size.height,
+        styles: customStyles,
+        width: size.width
+      })
+    : buildTemplate({ templateParams, size: _size });
 
   await page.setContent(html);
 
